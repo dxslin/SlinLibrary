5969888dc0762381cdbfa4ac8bd48c1daeef97a7
slin_huibo <dxslin@qq.com>
2020-02-13 22:56:28 +0800

    添加ViewPagerIndicator

---
+++ app/src/main/java/com/slin/slindialog/BlankFragment.java
@@ -0,0 +1,53 @@
+package com.slin.slindialog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by slin on 2020/2/13
+ */
+public class BlankFragment extends Fragment {
+
+    private static final String ARG_HELLO_TEXT = "param1";
+
+    private String mHelloText;
+
+    public BlankFragment() {
+
+    }
+
+    public static BlankFragment newInstance(String param1) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_HELLO_TEXT, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mHelloText = getArguments().getString(ARG_HELLO_TEXT);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        TextView tv_helloText = view.findViewById(R.id.tv_helloText);
+        tv_helloText.setText(mHelloText);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/slin/slindialog/MainActivity.java
+++ app/src/main/java/com/slin/slindialog/MainActivity.java
@@ -5,17 +5,51 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.slin.dialog.SlinDialog;
 import com.slin.dialog.core.DialogViewHolder;
 import com.slin.dialog.viewholder.ConfirmDialogViewHolder;
+import com.slin.indicator.ViewPagerIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String title[] = {"销售", "php", "汽车电子开发工程师", "Android", "汽车项目管理", "厂长", "建筑设计师", "服务员"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPagerIndicator vpi_advertisingJob = findViewById(R.id.vpi_advertisingJob);
+        ViewPager vp_resumeListPage = findViewById(R.id.vp_resumeListPage);
+        vpi_advertisingJob.setTabTexts(title);
+        vpi_advertisingJob.bindViewPager(vp_resumeListPage);
+
+        List<Fragment> fragments = new ArrayList<>(title.length);
+        for (int i = 0; i < title.length; i++) {
+            fragments.add(BlankFragment.newInstance(title[i]));
+        }
+
+        vp_resumeListPage.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+
+
     }
 
     public void onConfirmDialogClick(View view) {
