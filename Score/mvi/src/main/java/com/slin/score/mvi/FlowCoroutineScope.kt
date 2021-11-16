b0ad23cf11c9d00c3f6e84ac98f933e1518ba051
slin_huibo <dxslin@qq.com>
2021-11-16 13:46:09 +0800

    [doc] FlowCoroutineScopeImpl仅内部可见

--- Score/mvi/src/main/java/com/slin/score/mvi/FlowCoroutineScope.kt
+++ Score/mvi/src/main/java/com/slin/score/mvi/FlowCoroutineScope.kt
@@ -11,10 +11,9 @@ import kotlinx.coroutines.flow.FlowCollector
  * [FlowCollector]和[CoroutineScope]的代理Scope，可以在协程中获取数据之后调用[FlowCollector.emit]方法提交
  *
  */
-
 interface FlowCoroutineScope<S : ViewState> : FlowCollector<S>, CoroutineScope
 
-class FlowCoroutineScopeImpl<S : ViewState>(
+internal class FlowCoroutineScopeImpl<S : ViewState>(
     private val flowCollector: FlowCollector<S>,
     private val coroutineScope: CoroutineScope
 ) : FlowCoroutineScope<S>,
